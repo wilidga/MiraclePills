9b55b41ad289dc38d3b2bab94374c58fa1f1ee15
wilman garcia de leon <wilidga@gmail.com>
2017-08-27 21:29:59 -0600

    Second time

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var stateBtnPicker: UIButton!
+    
+    
+    
+ 
+    
+    let states = ["Alaska","Arkansas", "Alabama", "San Francisco", "Yew York"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self;
+        statePicker.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        statePicker.isHidden = false
+        
+//        countryLabel.isHidden = true
+//        zipCodeLabel.isHidden = true
+//        textFieldCountry.isHidden = true
+//        textFieldZipCode.isHidden = true
+        
+        
+        
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        stateBtnPicker.setTitle(states[row], for: UIControlState())//UIControlState.normal
+        statePicker.isHidden = true
+        
+//        countryLabel.isHidden = false
+       
+        
+    }
 
 }
 
